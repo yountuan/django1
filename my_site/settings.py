f8a5359e68d23e21e8f075d9aba809dbd531b6aa
Yountuan <aida.osmonova02@gmail.com>
2023-05-16 02:05:25 +0600

    gitignore issues

--- my_site/settings.py
+++ my_site/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
         'USERNAME': config('DB_USERNAME'),
         'PASSWORD': config('DB_PASSWOR'),
